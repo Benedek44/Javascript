e092c2881f03a645a98543c04928d928bab69d88
Benedek44 <144097374+Benedek44@users.noreply.github.com>
2024-09-29 15:01:39 +0200

    Update average.js

--- AverageOfDigits/average.js
+++ AverageOfDigits/average.js
@@ -1,14 +1,14 @@
 function Calculate(event)
 {
     event.preventDefault(); 
-    const number = parseInt(document.getElementById("Number").value);
+    const number = document.getElementById("Number").value;
 
     let average = 0;
     let numberLength = number.toString().length;
 
     for (let i = 0; i < numberLength; i++)
     {
-        average += parseInt(number.toString().charAt(i));
+        average += parseInt(number.charAt(i));
     }
     document.getElementById("result").innerHTML += `The average of digits is: ${average / numberLength} <br>`;
     document.getElementById('submitBtn').disabled = true;
